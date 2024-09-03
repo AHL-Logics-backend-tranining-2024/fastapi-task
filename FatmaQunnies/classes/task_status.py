6f5dac2fd2ed45d4783062362c92f6796c4bbaa6
FatmaQunnies24 <fatima.n.qunnies@gmail.com>
2024-09-03 20:37:01 +0300

    done

--- FatmaQunnies/classes/task_status.py
+++ FatmaQunnies/classes/task_status.py
@@ -2,3 +2,9 @@ from enum import Enum
 class Status(Enum):
     IN_PROGRESS = 'INPROGRESS'
     COMPLETED = 'COMPLETED'
+
+
+class Priority(Enum):
+    low = "low"
+    medium = "medium"
+    high = "high"   
