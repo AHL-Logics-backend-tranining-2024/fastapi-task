8c8c1f685fec21b054daf66bc64f0865cfbd2d73
FatmaQunnies24 <fatima.n.qunnies@gmail.com>
2024-09-04 15:46:08 +0300

    use dict

--- FatmaQunnies/app/route.py
+++ FatmaQunnies/app/route.py
@@ -2,15 +2,15 @@ from FatmaQunnies.classes import urgent_task
 from fastapi import FastAPI
 import uuid
 from classes import *
-from classes.model import TaskModel ,TaskUpdate
+from classes.model import TaskModel, TaskUpdate
 
 
 app = FastAPI()
-tasks = []
+tasks = {}
 
 @app.post("/tasks/")
 def create_task(task: TaskModel):
-    task_id = str(uuid.uuid4()) 
+    task_id = str(uuid.uuid4())  
     if task.priority:
         new_task = urgent_task(task.title, task.description, task.due_date, task.priority)
     else:
@@ -22,15 +22,15 @@ def create_task(task: TaskModel):
             "status": task.status,
             "priority": task.priority
         }
-    tasks.append(new_task)
+    tasks[task_id] = new_task 
     return new_task
 
 
 @app.put("/tasks/{task_id}/")
 def update_task(task_id: str, task_update: TaskUpdate):
-    task = next((t for t in tasks if t["id"] == task_id), None)
+    task = tasks.get(task_id) 
     if task is None:
-        raise  Exception
+        raise Exception
 
     if task_update.title is not None:
         task["title"] = task_update.title
@@ -43,32 +43,31 @@ def update_task(task_id: str, task_update: TaskUpdate):
     if task_update.priority is not None:
         task["priority"] = task_update.priority
 
+    tasks[task_id] = task
     return task
-
 
 
 @app.get("/tasks/")
 def get_all_tasks():
-    return tasks
+    return list(tasks.values()) 
 
 @app.get("/tasks/{task_id}/")
-def get_task_by_id(task_id: int):
-    for task in tasks:
-        if task.task_id == task_id:
-            return task
+def get_task_by_id(task_id: str):
+    task = tasks.get(task_id) 
+    if task:
+        return task
     raise Exception
+
 
 @app.get("/tasks/urgent/")
 def get_urgent_tasks():
-    urgent_tasks = [t for t in tasks if t.get("priority") is not None]
+    urgent_tasks = [t for t in tasks.values() if t.get("priority") is not None]  
     return urgent_tasks
-  
+
 
 @app.delete("/tasks/{task_id}/")
-def delete_task(task_id: int):
-    global tasks
-    for task in tasks:
-        if task.task_id == task_id:
-            tasks.remove(task)
-            return {"message": "Task deleted successfully"}
+def delete_task(task_id: str):
+    if task_id in tasks: 
+        del tasks[task_id] 
+        return {"message": "Task deleted successfully"}
     raise Exception
